36847e8dd86d5e229d1e3b0977f61f705ca67ccc
Gor <7495500@gmail.com>
2023-05-27 22:20:54 +0300

    Add tasks

--- rails-basics-ru/templates/test/controllers/tasks_controller_test.rb
+++ rails-basics-ru/templates/test/controllers/tasks_controller_test.rb
@@ -1,64 +1,48 @@
-# frozen_string_literal: true
-
-require 'test_helper'
+require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
   setup do
     @task = tasks(:one)
-
-    @attrs = {
-      name: Faker::Artist.name,
-      description: Faker::Movies::HarryPotter.quote,
-      status: Faker::Movies::HarryPotter.spell,
-      creator: Faker::Movies::HarryPotter.character,
-      performer: Faker::Movies::HarryPotter.character,
-      completed: Faker::Boolean.boolean
-    }
   end
 
-  test 'should get index' do
+  test "should get index" do
     get tasks_url
     assert_response :success
   end
 
-  test 'should get new' do
+  test "should get new" do
     get new_task_url
     assert_response :success
   end
 
-  test 'should create task' do
-    post tasks_url, params: { task: @attrs }
+  test "should create task" do
+    assert_difference("Task.count") do
+      post tasks_url, params: { task: { completed: @task.completed, creator: @task.creator, description: @task.description, name: @task.name, performer: @task.performer, status: @task.status } }
+    end
 
-    task = Task.find_by @attrs
-
-    assert { task }
-    assert_redirected_to task_url(task)
+    assert_redirected_to task_url(Task.last)
   end
 
-  test 'should show task' do
+  test "should show task" do
     get task_url(@task)
     assert_response :success
   end
 
-  test 'should get edit' do
+  test "should get edit" do
     get edit_task_url(@task)
     assert_response :success
   end
 
-  test 'should update task' do
-    patch task_url(@task), params: { task: @attrs }
-
-    @task.reload
-
-    assert { @task.name == @attrs[:name] }
+  test "should update task" do
+    patch task_url(@task), params: { task: { completed: @task.completed, creator: @task.creator, description: @task.description, name: @task.name, performer: @task.performer, status: @task.status } }
     assert_redirected_to task_url(@task)
   end
 
-  test 'should destroy task' do
-    delete task_url(@task)
+  test "should destroy task" do
+    assert_difference("Task.count", -1) do
+      delete task_url(@task)
+    end
 
     assert_redirected_to tasks_url
-
-    assert { !Task.exists? @task.id }
   end
 end

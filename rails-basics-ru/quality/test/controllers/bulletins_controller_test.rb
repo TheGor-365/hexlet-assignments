b7748bf415c6f11052bf992cafd5762ee808f3d9
Gor <7495500@gmail.com>
2023-05-29 15:59:27 +0300

    Add tests

--- rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
+++ rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
@@ -1,1 +1,16 @@
 # frozen_string_literal: true
+require "test_helper"
+
+class BulletinsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get bulletins_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    @bulletine = bulletins(:one)
+
+    get bulletine_url(@bulletine)
+    assert_response :success
+  end
+end

20f79a60d2921eb57fd8a8b3d82eebcce763a14d
Gor <7495500@gmail.com>
2023-05-29 13:56:18 +0300

    Fix tests bug

--- rails-basics-ru/models/config/routes.rb
+++ rails-basics-ru/models/config/routes.rb
@@ -3,8 +3,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   # BEGIN
-  get 'articles/index', to: 'articles#index'
-  get 'articles/show/:id', to: 'articles#show', as: 'articles_show'
+  resources :articles, only: [:index, :show]
   # END
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

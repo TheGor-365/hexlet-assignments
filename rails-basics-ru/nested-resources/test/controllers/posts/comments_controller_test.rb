7f08d11b5c4fa6049679f60d0997f8d2e8149ecb
Gor <7495500@gmail.com>
2023-06-14 17:53:30 +0300

    Fix update test for comments controller 5

--- rails-basics-ru/nested-resources/test/controllers/posts/comments_controller_test.rb
+++ rails-basics-ru/nested-resources/test/controllers/posts/comments_controller_test.rb
@@ -1,56 +1,55 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-module Post  
-  class CommentsControllerTest < ActionDispatch::IntegrationTest
-    setup do
-      @comment = post_comments(:one)
-      @post = post(:one)
-    end
 
-    test 'should get new' do
-      get new_post_comment_url
-      assert_response :success
-    end
+class Post::CommentsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @comment = post_comments(:one)
+    @post = post(:one)
+  end
 
-    test 'should create post_comment' do
-      assert_difference("PostComment.count") do
-        post post_comments_url(@post), params: {
-          post_comment: {
-            body:    @comment.body,
-            post_id: @comment.post_id
-          }
-        }
-      end
+  test 'should get new' do
+    get new_post_comment_url
+    assert_response :success
+  end
 
-      assert_redirected_to post_url(@post)
-    end
-
-    test 'should show post_comment' do
-      get post_url(@post)
-      assert_response :success
-    end
-
-    test 'should get edit' do
-      get edit_post_comment(@post, @comment)
-      assert_response :success
-    end
-
-    test 'should update post_comment' do
-      patch post_comment_path(@post), params: {
+  test 'should create post_comment' do
+    assert_difference("PostComment.count") do
+      post post_comments_url(@post), params: {
         post_comment: {
           body:    @comment.body,
           post_id: @comment.post_id
         }
       }
-      assert_redirected_to post_url(@post)
     end
 
-    test 'should destroy post_comment' do
-      delete post_comment(@post, @comment)
+    assert_redirected_to post_url(@post)
+  end
 
-      assert { !PostComment.exists?(@comment.id) }
-      assert_redirected_to posts_url
-    end
+  test 'should show post_comment' do
+    get post_url(@post)
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get edit_post_comment(@post, @comment)
+    assert_response :success
+  end
+
+  test 'should update post_comment' do
+    patch post_comment_path(@post), params: {
+      post_comment: {
+        body:    @comment.body,
+        post_id: @comment.post_id
+      }
+    }
+    assert_redirected_to post_url(@post)
+  end
+
+  test 'should destroy post_comment' do
+    delete post_comment(@post, @comment)
+
+    assert { !PostComment.exists?(@comment.id) }
+    assert_redirected_to posts_url
   end
 end

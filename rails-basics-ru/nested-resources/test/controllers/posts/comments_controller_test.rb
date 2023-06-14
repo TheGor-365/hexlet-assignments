4a724e87abeb9f0a98097fa8d7ef4511de58b6bc
Gor <7495500@gmail.com>
2023-06-14 18:32:06 +0300

    Fix pathes for comments controller tests 4

--- rails-basics-ru/nested-resources/test/controllers/posts/comments_controller_test.rb
+++ rails-basics-ru/nested-resources/test/controllers/posts/comments_controller_test.rb
@@ -10,11 +10,15 @@ module Posts
     end
 
     test 'should get new' do
+      @post = post(:one)
+
       get new_post_comment_path(@post)
       assert_response :success
     end
 
     test 'should create post_comment' do
+      @post = post(:one)
+
       assert_difference("PostComment.count") do
         post post_comments_path(@post), params: {
           post_comment: {
@@ -28,16 +32,22 @@ module Posts
     end
 
     test 'should show post_comment' do
+      @post = post(:one)
+
       get post_path(@post)
       assert_response :success
     end
 
     test 'should get edit' do
+      @post = post(:one)
+
       get edit_post_comment_path(@post, @comment)
       assert_response :success
     end
 
     test 'should update post_comment' do
+      @post = post(:one)
+
       patch post_comment_path(@post), params: {
         post_comment: {
           body:    @comment.body,
@@ -48,6 +58,8 @@ module Posts
     end
 
     test 'should destroy post_comment' do
+      @post = post(:one)
+      
       delete post_comment_path(@post, @comment)
 
       assert { !PostComment.exists?(@comment.id) }

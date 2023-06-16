85792d72b50b7957ba4e214b31ac69bf05884df2
Gor <7495500@gmail.com>
2023-06-16 18:18:24 +0300

    Add translations with i18n library

--- rails-basics-ru/i18n/config/routes.rb
+++ rails-basics-ru/i18n/config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     root 'home#index'
 
     resources :posts do
-      resources :comments, only: %i[edit create update destroy]
+      scope module: :posts do
+        resources :comments, only: %i[edit create update destroy]
+      end
     end
   # end
   # END

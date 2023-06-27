cfd6692f6aea8e3d81baa6441da950cfda860e98
Gor <7495500@gmail.com>
2023-06-27 16:56:36 +0300

    Add switch locale

--- rails-basics-ru/middlewares/config/application.rb
+++ rails-basics-ru/middlewares/config/application.rb
@@ -36,6 +36,9 @@ module Middlewares
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # config.middleware.use SetLocaleMiddleware
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :en
+
+    config.middleware.use SetLocaleMiddleware
   end
 end

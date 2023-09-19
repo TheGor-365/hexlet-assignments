4fa03a6653e135c164e95c0515c4ceca5ade8c20
Gor <7495500@gmail.com>
2023-09-19 22:34:20 +0300

    Add integration tests for Posts

--- rails-real-ru/system-tests/test/application_system_test_case.rb
+++ rails-real-ru/system-tests/test/application_system_test_case.rb
@@ -3,7 +3,8 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |driver|
+  # driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |driver|
+  driven_by :selenium, using: :firefox, screen_size: [1400, 1400] do |driver|
     driver.add_argument('--no-sandbox')
     driver.add_argument('--disable-gpu')
   end
--- rails-real-ru/system-tests/test/system/posts_test.rb
+++ rails-real-ru/system-tests/test/system/posts_test.rb
@@ -3,5 +3,51 @@
 require 'application_system_test_case'
 
 # BEGIN
+class PostsTest < ApplicationSystemTestCase
+  setup do
+    @post = posts(:one)
+    @post_two = posts(:two)
+    @post_without_comments = posts(:without_comments)
+  end
 
+  test 'visiting the index' do
+    visit posts_url
+
+    assert_selector 'h1', text: 'Posts'
+  end
+
+  test 'should create post' do
+    visit posts_url
+    click_on 'New Post'
+
+    fill_in 'Title', with: @post.title
+    fill_in 'Body', with: @post.body
+    click_on 'Create Post'
+
+    assert_text "Post was successfully created"
+    take_screenshot
+    click_on "Back"
+  end
+
+  test "should update post" do
+    visit posts_url
+    click_on "Edit", match: :first
+
+    fill_in "Title", with: @post_two.title
+    fill_in "Body", with: @post_two.body
+    click_on "Update Post"
+
+    assert_text "Post was successfully updated"
+    click_on "Back"
+  end
+
+  test "should delete post" do
+    visit posts_url
+    click_on "Destroy", match: :first
+
+    accept_alert do
+      'Are you sure?'
+    end
+  end
+end
 # END

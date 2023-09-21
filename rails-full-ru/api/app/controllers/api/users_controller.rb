966013872d2b76ce7e513ac2a5e1ce471643dcef
Gor <7495500@gmail.com>
2023-09-21 19:28:17 +0300

    Add api controller actions and routes

--- rails-full-ru/api/app/controllers/api/users_controller.rb
+++ rails-full-ru/api/app/controllers/api/users_controller.rb
@@ -2,6 +2,20 @@
 
 class Api::UsersController < Api::ApplicationController
   # BEGIN
-  
+  def show
+    @user = User.find(params[:id])
+
+    respond_with @user.as_json(
+      root: true
+    )
+  end
+
+  def index
+    @users = User.all
+
+    respond_with @users.as_json(
+      root: true
+    )
+  end
   # END
 end

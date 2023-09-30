67187f2880a8b9d20cfc7437543ffe2bf1b9a173
@hexlet/cli <support@hexlet.io>
2023-09-30 22:18:37 +0300

    submit ruby-basics-ru/collections

---
+++ ruby-basics-ru/collections/lib/compare_versions.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# BEGIN
+def compare_versions(ver1, ver2)
+  major1, minor1 = ver1.split('.').map(&:to_i)
+  major2, minor2 = ver2.split('.').map(&:to_i)
+
+  major1.equal?(major2) ? minor1 <=> minor2 : major1 <=> major2
+end
+# END

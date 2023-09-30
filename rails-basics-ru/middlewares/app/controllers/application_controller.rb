54d887c884e5fd1d08147069cf2827c0c0d4b275
@hexlet/cli <support@hexlet.io>
2023-09-30 21:53:38 +0300

    submit rails-basics-ru/middlewares

---
+++ rails-basics-ru/middlewares/app/controllers/application_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  # def switch_locale(&action)
+  #   logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+  #   locale = extract_locale_from_accept_language_header
+  #   logger.debug "* Locale set to '#{locale}'"
+  #   I18n.with_locale(locale, &action)
+  # end
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  # def extract_locale_from_accept_language_header
+  #   request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  # end
+end
---
+++ rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class SetLocaleMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  # BEGIN
+  def call(env)
+    @status, @headers, @response = @app.call env
+
+    [@status, @headers, @response]
+  end
+  # END
+end

dbcd16337bc13a4ed8076ff6a1e43a8735122508
Gor <7495500@gmail.com>
2023-09-30 14:49:14 +0300

    Add caching

--- rails-full-ru/caching/app/models/article.rb
+++ rails-full-ru/caching/app/models/article.rb
@@ -2,6 +2,10 @@
 
 class Article < ApplicationRecord
   # BEGIN
-  
+  def last_reading_date
+    Rails.cache.fetch("#{cache_key_with_version}/last_reading_date", expires_in: 12.hours) do
+      DateTime.now
+    end
+  end
   # END
 end

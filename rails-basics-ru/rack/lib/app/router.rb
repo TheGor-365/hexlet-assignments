af4415f8de14bfb70e4d795b513023854bb67fb8
@hexlet/cli <support@hexlet.io>
2023-09-30 22:00:19 +0300

    submit rails-basics-ru/rack

---
+++ rails-basics-ru/rack/lib/app/router.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Router
+  def call(env)
+    # BEGIN
+    req = Rack::Request.new(env)
+
+    case req.path_info
+    when '/'
+      [200, { 'Content-Type' => 'text/html' }, ['Hello, World!']]
+    when '/about'
+      [200, { 'Content-Type' => 'text/html' }, ['About page']]
+    else
+      [404, { 'Content-Type' => 'text/html' }, ['404 Not Found']]
+    end
+    # END
+  end
+end
